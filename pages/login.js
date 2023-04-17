3c3c5b8bae7f3dbc8b61b4ca3c26cf22bb601162
amanpsharma <14mcaaman@gmail.com>
2023-04-17 17:28:23 +0530

    added margin

--- pages/login.js
+++ pages/login.js
@@ -78,7 +78,7 @@ const LoginForm = () => {
 								required
 							/>
 						</div>
-						{error && <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+						{error && <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-5" role="alert">
 						<strong class="font-bold">Hey, </strong>
 						<span class="block sm:inline">{error}</span>
 						<span class="absolute top-0 bottom-0 right-0 px-4 py-3" onClick={()=>setError("")} >
