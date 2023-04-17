6e7705952b1c96e31eb1bb2c93264e106cbbf38f
amanpsharma <14mcaaman@gmail.com>
2023-04-17 17:24:54 +0530

    handle error for invalid user login

--- pages/login.js
+++ pages/login.js
@@ -13,7 +13,7 @@ const LoginForm = () => {
 
 	const [password, setPassword] = useState(null);
 	const [email, setEmail] = useState(null);
-
+	const [error, setError] = useState("");
 	const { authUser, isLoading } = useAuth();
 
 	const loginHandler = async () => {
@@ -21,8 +21,12 @@ const LoginForm = () => {
 		try {
 			const user = await signInWithEmailAndPassword(auth, email, password);
 		} catch (error) {
-			console.error(error);
+			setError(error.message);
+			console.log(error.message,'error');
 		}
+		setTimeout(() => {
+			setError("");
+		}, 5000);
 	};
 	useEffect(() => {
 		if (!isLoading && authUser) {
@@ -74,12 +78,20 @@ const LoginForm = () => {
 								required
 							/>
 						</div>
+						{error && <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+						<strong class="font-bold">Hey, </strong>
+						<span class="block sm:inline">{error}</span>
+						<span class="absolute top-0 bottom-0 right-0 px-4 py-3" onClick={()=>setError("")} >
+							<svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+						</span>
+						</div>}
 						<button
 							className="bg-black text-white w-44 py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90"
 							onClick={loginHandler}
 						>
 							Sign in
 						</button>
+						
 					</form>
 				</div>
 			</div>
