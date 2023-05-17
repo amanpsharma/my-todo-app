8e4aac5ec6dc064eb8f0c8b8a8447dd6949a1e47
amanpsharma <14mcaaman@gmail.com>
2023-05-17 15:59:36 +0530

    change title

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,14 @@ import { AuthUserProvider } from "@/firebase/auth";
 import "@/styles/globals.css";
 import Head from "next/head";
 export default function App({ Component, pageProps }) {
-	return (
-		<>
-			<Head>
-				<title>JS Dev - Todo App</title>
-			</Head>
-			<AuthUserProvider>
-				<Component {...pageProps} />
-			</AuthUserProvider>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Todo App</title>
+      </Head>
+      <AuthUserProvider>
+        <Component {...pageProps} />
+      </AuthUserProvider>
+    </>
+  );
 }
